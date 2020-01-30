8f1cd4eccdd75de1da77dd3a5394a37b8503715e
Malte Biermann <51059791+MalteBi@users.noreply.github.com>
2020-01-30 18:29:24 +0100

    class Winkel modified; dep. winkelrechner modified

--- gui/winkelrechner.py
+++ gui/winkelrechner.py
@@ -4,26 +4,27 @@ if __name__ == "__main__":
     import sys
     sys.path.append(".")
 
-from operation.winkelrechner import *
+from datentyp.winkel import Winkel
+
 
 class FensterWinkelrechner(Frame):
     def __init__(self, master):
         super().__init__(master)
 
-        self.eingabe1 = StringVar()
-        self.eingabe2 = StringVar()
-        self.eingabe3 = StringVar()
+        self.strEntryGrad = StringVar()
+        self.strEntryRad = StringVar()
+        self.strEntryGon = StringVar()
         self.grid()
 
         Label(self, text="Grad:").grid(row=0, column=0)
         Label(self, text="Rad:").grid(row=1, column=0)
         Label(self, text="Gon:").grid(row=2, column=0)
 
-        Entry(self, textvariable=self.eingabe1).grid(
+        Entry(self, textvariable=self.strEntryGrad).grid(
             row=0, column=1, padx=3, pady=3)
-        Entry(self, textvariable=self.eingabe2).grid(
+        Entry(self, textvariable=self.strEntryRad).grid(
             row=1, column=1, padx=3, pady=3)
-        Entry(self, textvariable=self.eingabe3).grid(
+        Entry(self, textvariable=self.strEntryGon).grid(
             row=2, column=1, padx=3, pady=3)
 
         Button(self, text="Berechne", command=self.btnPressedBerechneGrad).grid(
@@ -38,23 +39,19 @@ class FensterWinkelrechner(Frame):
         self.wait_window()
 
     def btnPressedBerechneGrad(self):
-        #m = "rad={:5.3f}".format(grad2rad(float(self.eingabe1.get())))
-        gon = grad2gon(float(self.eingabe1.get().replace(",", ".")))
-        rad = grad2rad(float(self.eingabe1.get().replace(",", ".")))
-        self.eingabe2.set(rad)
-        self.eingabe3.set(gon)
+        w = Winkel(float(self.strEntryGrad.get().replace(",", ".")),einheit="grad")
+        self.strEntryRad.set(w.get_w(einheit="rad"))
+        self.strEntryGon.set(w.get_w(einheit="gon"))
 
     def btnPressedBerechneRad(self):
-        gon = rad2gon(float(self.eingabe2.get().replace(",", ".")))
-        grad = rad2grad(float(self.eingabe2.get().replace(",", ".")))
-        self.eingabe3.set(gon)
-        self.eingabe1.set(grad)
+        w = Winkel(float(self.strEntryRad.get().replace(",", ".")),einheit="rad")
+        self.strEntryGon.set(w.get_w(einheit="gon"))
+        self.strEntryGrad.set(w.get_w(einheit="grad"))
 
     def btnPressedBerechneGon(self):
-        grad = gon2grad(float(self.eingabe3.get().replace(",", ".")))
-        rad = gon2rad(float(self.eingabe3.get().replace(",", ".")))
-        self.eingabe2.set(rad)
-        self.eingabe1.set(grad)
+        w = Winkel(float(self.strEntryGon.get().replace(",", ".")),einheit="gon")
+        self.strEntryRad.set(w.get_w(einheit="rad"))
+        self.strEntryGrad.set(w.get_w(einheit="grad"))
 
 
 if __name__ == "__main__":
