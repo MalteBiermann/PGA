5e0a05a4bb2d6a6353f362149bb3b7d942e5b40f
Malte Biermann <51059791+MalteBi@users.noreply.github.com>
2020-02-09 14:05:02 +0100

    polygon class added

---
+++ datentyp/polygon.py
@@ -0,0 +1,68 @@
+if __name__ == "__main__":
+    import sys
+    sys.path.append(".")
+
+from datentyp.punkt import Punkt,Punkt_Dic
+
+# Punkt_Dic(ungeordnet) kombiniert mit einer Liste um Punkte in eine Reihenfolge zu bringen
+class Polygon:
+    def __init__(self):
+        self.__pIdList = []
+        self.__PDic = Punkt_Dic()
+    
+    def get_pIdList(self):
+        return self.__pIdList
+        
+    def add_Ppointwithcoords(self,p,attribute=None):
+        self.__PDic.addPoint(p)
+        self.__pIdList.append(p.get_id())
+        if attribute != None:
+            pId = self.__pIdList[-1]
+            self.__PDic.setAttribute(pId,attribute)
+
+    def update_PpointAttributebyNr(self, OrderNr, a):
+        pId = self.__pIdList[OrderNr]
+        self.__PDic.setAttribute(pId,a)
+
+    def update_PpointAttributebyID(self, sId, a):
+        self.__PDic.setAttribute(sId,a)
+
+    def add_NextPointByDirDist(self, dir, dist):
+        currentNr = len(self.__pIdList)
+        previousPId = self.__pIdList[-1]
+        p = self.__PDic.get_dic()[previousPId]["coord"].ersteHA(dist, dir)
+        p.set_id(currentNr)
+        self.add_Ppointwithcoords(p)
+
+    def get_PpointbyNr(self,OrderNr):
+        pId = self.__pIdList[OrderNr]
+        return self.get_PpointbyPId(pId)
+
+    def get_PpointbyPId(self,pId):
+        return self.__PDic.get_dic()[pId]
+
+    def get_counter(self):
+        return len(self.__pIdList)
+    
+    def get_pDic(self):
+        return self.__PDic.get_dic()
+
+
+if __name__ == "__main__":
+    from datentyp.winkel import Winkel
+    from datentyp.strecke import Strecke
+
+    p0 = Punkt(1,1,"p01")
+
+    dir01 = Winkel(50, "gon")
+    dist01 = Strecke.init_länge(3)
+
+    poly = Polygon()
+    poly.add_Ppointwithcoords(p0)
+    poly.add_NextPointByDirDist(dir01,dist01)
+    poly.add_NextPointByDirDist(dir01,dist01)
+    poly.update_PpointAttributebyNr(2,{"test": 1235})
+
+
+    print(poly.get_PpointbyNr(2))
+
