8c21ab3c3e6705a0229e1dc99b2fc68ab603e903
Malte Biermann <51059791+MalteBiermann@users.noreply.github.com>
2020-02-11 22:37:34 +0100

    authors added

--- gui/hauptfenster.py
+++ gui/hauptfenster.py
@@ -1,4 +1,5 @@
-from tkinter import Frame,Tk,Button,Toplevel
+from tkinter import Frame,Tk,Button,Toplevel,Label
+from tkinter.ttk import Separator
 
 if __name__ == "__main__":
     import sys
@@ -10,26 +11,33 @@ import gui.vorwärtsschnitt
 import gui.hauptaufgaben
 import gui.bogenschnitt
 import gui.transformation
+import gui.polygonzug
 
 class HauptFenster(Frame):
     def __init__(self, master):
         super().__init__(master)
         self.grid()
 
-        Button(self, text="Winkelrechner", command=self.openFensterWinkelrechner).grid(
+        Button(self, text="Winkelrechner", command=self.openFensterWinkelrechner, width=15).grid(
             row=1, column=0, padx=3, pady=3, sticky="w")
-        Button(self, text="Hauptaufgaben", command=self.openFensterHA).grid(
+        Button(self, text="Hauptaufgaben", command=self.openFensterHA, width=15).grid(
             row=2, column=0, padx=3, pady=3, sticky="w")
-        Button(self, text="Bogenschnitt", command=self.openFensterBS).grid(
+        Button(self, text="Bogenschnitt", command=self.openFensterBS, width=15).grid(
             row=3, column=0, padx=3, pady=3, sticky="w")
-        Button(self, text="Rückwärtsschnitt", command=self.openFensterRS).grid(
+        Button(self, text="Rückwärtsschnitt", command=self.openFensterRS, width=15).grid(
             row=3, column=1, padx=3, pady=3, sticky="w")
-        Button(self, text="Vorwärtsschnitt", command=self.openFensterVS).grid(
+        Button(self, text="Vorwärtsschnitt", command=self.openFensterVS, width=15).grid(
             row=3, column=2, padx=3, pady=3, sticky="w")
-        Button(self, text="Polygonzug", command=self.openFensterPZ).grid(
+        Button(self, text="Polygonzug", command=self.openFensterPZ, width=15).grid(
             row=4, column=0, padx=3, pady=3, sticky="w")
-        Button(self, text="Transformation", command=self.openFensterTrans).grid(
+        Button(self, text="Transformation", command=self.openFensterTrans, width=15).grid(
             row=5, column=0, padx=3, pady=3, sticky="w")
+        
+        Separator(self,orient="horizontal").grid(
+            row=6, column=0, padx=3, pady=3, columnspan=3, sticky="we")
+        
+        Label(self, text="Svenja Rode, Chris Arends, Hendrik Gebben und Malte Biermann").grid(
+            row=7, column=0, padx=3, pady=3, columnspan=3, sticky="")
 
     def openFensterWinkelrechner(self):
         top = Toplevel()
